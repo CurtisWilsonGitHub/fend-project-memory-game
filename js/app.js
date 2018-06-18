3afd9b196da671a1d20c2448d361a4ded6aceb31
Curtis Wilson <curtiswemail@gmail.com>
2018-06-17 21:35:52 -0600

    Add match_maker method, show_card method, and event listener that flips card

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+const deck = document.getElementById('play_area');
+let card_array = [];
 /*
  * Create a list that holds all of your cards
  */
@@ -25,6 +27,30 @@ function shuffle(array) {
     return array;
 }
 
+function show_card(){
+  event.target.className = "card open show";
+}
+
+function match_maker(card,card_array){
+  card_array.push(card);
+  if(card_array.length == 2){
+    let card_one = card_array[0].children;
+    let card_two = card_array[1].children;
+    console.log(card_one[0].className);
+    console.log(card_two[0].className);
+
+    if(card_one[0].className == card_two[0].className){
+      console.log('true');
+      card_one[0].parentElement.className="card match";
+      card_two[0].parentElement.className="card match";
+    } else{
+      console.log('false');
+      card_one[0].parentElement.className="card";
+      card_two[0].parentElement.className="card";
+    }
+    card_array.length = 0;
+  }
+}
 
 /*
  * set up the event listener for a card. If a card is clicked:
@@ -36,3 +62,12 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+deck.addEventListener('click',function(e){
+  if(e.target && e.target.matches("li.card")){
+    show_card();
+    setTimeout(function(){
+      match_maker(e.target, card_array)
+    },500);
+  }
+});
