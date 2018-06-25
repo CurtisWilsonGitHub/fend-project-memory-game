8d0267bd8225254645bb869df02644c42fb89c24
Curtis Wilson <curtiswemail@gmail.com>
2018-06-24 22:42:18 -0600

    Ran code through validator

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 /*
- * code related to the modal was modified from const modal = document.getElementById('winModal');
-  */
+ * code related to the modal was modified from https://www.w3schools.com/howto/howto_css_modals.asp
+ */
 
 const deck = document.getElementById('play_area');
 const modal = document.getElementById('winModal');
@@ -203,18 +203,8 @@ function printFinalTime(){
 }
 
 
-
-
-
 /*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+ * Event Listeners
  */
 
 deck.addEventListener('click',function(e){
